8d32ca3e4aef2e8ded53ec25ea6d1e4ca6fe6eea
ballyees <shouldertip@gmail.com>
2022-06-02 18:33:00 +0700

    client-site

--- server/controllers/employee.go
+++ server/controllers/employee.go
@@ -10,28 +10,43 @@ import (
 )
 
 type EmployeePVD struct {
-	Employee model.Employee
-	TotalPVD float64
-}
-
-func getSubMonth(month, max int) int {
-	if month > max {
-		return month - max
-	}
-	return 0
+	Employee      model.Employee
+	TotalPVD      float64
+	TotalMonthPVD float64
 }
 
 func CalculatePVD(month float64, paidRate float64, employee model.Employee) float64 {
 	return ((employee.Salary * paidRate / 100) * month) + ((employee.Salary * employee.PvdFundRate / 100) * month)
 }
 
-func CalculateTotalPVD(employee model.Employee, now time.Time) float64 {
+func CalculateTotalMonthPVD(employee model.Employee, now time.Time) (totalPVD float64) {
 	t, _ := time.Parse(TimeFormat, employee.StartDate)
 	diff := now.Sub(t)
 	year := diff.Hours() / (24 * 365)
 	month := math.Floor((year - math.Floor(year)) * 12)
 	year = math.Floor(year)
-	var totalPVD float64
+	totalMonth := ((year * 12) + month) - 3
+	if totalMonth <= 0 {
+		return
+	}
+	if year > 5 { // over 5 year
+		totalPVD = CalculatePVD(totalMonth, 80, employee)
+	} else if year > 3 { // less than 5 year
+		totalPVD = CalculatePVD(totalMonth, 50, employee)
+	} else if year > 1 { // less than 3 year
+		totalPVD = CalculatePVD(totalMonth, 30, employee)
+	} else if year >= 0 { // less than 1 year
+		totalPVD = CalculatePVD(totalMonth, 10, employee)
+	}
+	return
+}
+
+func CalculateTotalPVD(employee model.Employee, now time.Time) (totalPVD float64) {
+	t, _ := time.Parse(TimeFormat, employee.StartDate)
+	diff := now.Sub(t)
+	year := diff.Hours() / (24 * 365)
+	month := math.Floor((year - math.Floor(year)) * 12)
+	year = math.Floor(year)
 	if year >= 5 { // over 5 year
 		totalPVD += CalculatePVD(12*(year-5)+month, 80, employee)
 		year = 5
@@ -66,7 +81,9 @@ func GetEmployeePVD(db *sql.DB) func(c *fiber.Ctx) error {
 		for i, d := range data {
 			employeePVDs[i].Employee = d
 			employeePVDs[i].TotalPVD = CalculateTotalPVD(d, now)
+			employeePVDs[i].TotalMonthPVD = CalculateTotalMonthPVD(d, now)
 		}
+		time.Sleep(time.Duration(10) * time.Second)
 		return c.JSON(employeePVDs)
 	}
 }
